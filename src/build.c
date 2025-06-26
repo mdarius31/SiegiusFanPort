3fdaf081144978d1aaba5b760471fe1662c8c92c
mdarius31 <darius31murariu@gmail.com>
2025-06-26 19:33:13 +0300

    creating a window works

--- src/build.c
+++ src/build.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 int main(void) {
-	const char* cmd = "x86_64-w64-mingw32-gcc main.c -o SiegiusFanPort.exe";
+
+	const char* cmd = "x86_64-w64-mingw32-gcc main.c -o SiegiusFanPort.exe ./lib/raylib/src/libraylib.a -lm -lwinmm -lgdi32 -lopengl32 -mwindows";
 	printf("%s\n", cmd);
 	system(cmd);	
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include "lib/raylib/src/raylib.h"
+
+
 
 int main(void) {
- printf("Hello from SiegiusFanPort!\n");
+ const int screenWidth = 700;
+ const int screenHeight = 400;
+ SetTraceLogLevel(LOG_NONE);
+
+ InitWindow(screenWidth, screenHeight, "SiegiusFanPort");
+
+ SetWindowState(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+
+ SetExitKey(KEY_NULL);
+
+ bool shouldClose = false;
+
+ while(!(WindowShouldClose() || shouldClose)) {
+  BeginDrawing();
+  ClearBackground(GRAY);
+  EndDrawing();
+ }
+ 
  
  return 0;
 }
